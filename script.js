2e7e862e0c2c97ea25894acd052641b4025812fd
gituser31 <98166828+gituser31@users.noreply.github.com>
2025-09-22 08:36:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tombol = document.getElementById("btn-alert");
-  if (tombol) {
-    tombol.addEventListener("click", function () {
-      alert("Halo, ini berhasil dari file JS di GitHub!");
-    });
-  }
+  $("#btn-alert").click(function(){
+    alert("halo");
+  });
 });
